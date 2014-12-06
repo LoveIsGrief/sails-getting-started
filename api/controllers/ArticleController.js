b090728d82857667a5eda27e0e3c3db22b298517
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2014-12-06 15:21:59 +0100

    Add ArticleController::show
    For finding a given article (with given id) and handling the db response

--- api/controllers/ArticleController.js
+++ api/controllers/ArticleController.js
@@ -13,6 +13,20 @@ module.exports = {
                 return res.serverError(err.toString())
             }else{
                 return res.redirect('/article/' + created.id)
+            })
+    },
+
+    show: function (req, res) {
+        id = req.param('id')
+        Article.findOne({ "id": id}, function(error, article){
+            // Model.find doesn't consider attempting to find a non-existent object
+            // a problem and simply returns no error and undefined
+            if (error || article == undefined) {
+                res.notFound('Article with id: ' + id)
+            } else{
+                res.view('article/show', {
+                    "article": article
+                })
             }
         })
     }
