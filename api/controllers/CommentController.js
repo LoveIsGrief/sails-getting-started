12b46c145c0de8b4c2ae1b6d47af451357ff675f
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2014-12-29 15:16:27 +0100

    Add CommentController.destroy
    Now specific comments can be deleted by id

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -12,6 +12,19 @@ module.exports = {
 		Comment.create(inputComment, function (error, created) {
 				res.redirect('/article/' + inputComment.commentedArticle)
 		})
+	},
+
+	destroy: function (req, res) {
+		id = req.param('id')
+		params = req.allParams()
+		Comment.destroy(
+			id, // Article to destroy
+			function (error,comments) {
+				if (comments.length > 0) {
+					articleId = comments[0].commentedArticle
+					res.redirect('/article/'+articleId)
+				}
+		})
 	}
 };
 
